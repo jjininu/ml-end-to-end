be2cdea1a804a3a16f0b911f503f2d8333472aa8
Junior Eshetu <76874506+jjininu@users.noreply.github.com>
2022-06-16 18:32:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return render_template('home.html')
-
+    return "this new"
 
 if __name__=="__main__":
     app.run(debug=True)
